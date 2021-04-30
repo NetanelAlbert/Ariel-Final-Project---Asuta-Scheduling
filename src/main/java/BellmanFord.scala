09ff120f11fff021bda44d226451fbcefd76ab8d
Nati Albert <netanela@carbyne911.com>
2021-04-30 12:00:04 +0300

    Initial commit

---
+++ src/main/java/BellmanFord.scala
@@ -0,0 +1,63 @@
+object BellmanFord {
+    def initialize(distribution : RandomVariable, source : Int) =
+    {
+        val graph = new MutableGraph(distribution.support + Int.MaxValue)
+        val nodes = graph.vertices.toSeq.sorted
+        for(i <- nodes.indices)
+        {
+            val nodeI = nodes(i)
+            for(j <- i+1 until nodes.size)
+            {
+                val nodeJ = nodes(j)
+                graph.connect(nodeI, nodeJ, distribution.cumulativePRangeExclude(nodeI, nodeJ))
+            }
+        }
+        graph.setDistance(source, 0)
+        graph
+    }
+    
+    
+    
+    def bellman_ford(graph: MutableGraph, source : Int, length : Int) : Seq[Int] =
+    {
+        def relax(node : Int)
+        {
+            for(ng <- graph.neighborsOf(node))
+            {
+                val maxi = Math.max(graph.distanceOf(node), graph.weightOf(node, ng))
+                if (maxi < graph.distanceOf(ng))
+                {
+                    graph.setDistance(ng, maxi)
+                    graph.setParent(ng, node)
+                }
+            }
+        }
+ 
+        //g = topological(g)
+        // todo - do we need topological? I think we will get the natural (numbering) order
+        val nodes = graph.vertices.toSeq.sorted
+        for(_ <- 1 to length; node <- nodes)
+        {
+            relax(node)
+        }
+    
+    
+        def traceFromSource(node: Int) : Seq[Int] =
+        {
+            val parent = graph.parentOf(node)
+            parent match
+            {
+                case None => Seq(node)
+                case Some(num) => traceFromSource(num) :+ node
+            }
+        }
+        
+        val lastNode = graph.parentOf(Int.MaxValue)
+        lastNode match
+        {
+            case None => throw new RuntimeException("Infinity node is not reached")
+            case Some(num) => traceFromSource(num)
+        }
+    }
+
+}
---
+++ src/main/java/MutableGraph.scala
@@ -0,0 +1,118 @@
+import java.awt.Color
+import scala.collection.mutable
+
+class MutableGraph(nodes : Set[Int])
+{
+    private val m_nodes = new mutable.HashMap[Int, Node]() ++
+        nodes.map(node => node -> Node(node))
+    
+    def this(){
+        this(Set())
+    }
+    
+    def += (num : Int)
+    {
+        m_nodes += num -> Node(num)
+    }
+    
+    def vertices = m_nodes.keySet
+    
+    def connect(src : Int, dest : Int, weight : Double)
+    {
+        m_nodes(src).m_neighbors += dest -> Edge(src, dest, weight)
+    }
+    
+    def isConnected(src : Int, dest : Int) =
+    {
+        m_nodes(src).m_neighbors.contains(dest)
+    }
+    
+    def weightOf(src : Int, dest : Int) =
+    {
+        m_nodes(src).m_neighbors(dest).m_weight
+    }
+    
+    def neighborsOf(node : Int) =
+    {
+        m_nodes(node).m_neighbors.keySet
+    }
+    
+    def setParent(node : Int, parent : Int)
+    {
+        if(isConnected(parent, node))
+            m_nodes(node).m_parent = Some(parent)
+        else
+            throw new RuntimeException("No such element: "+parent)
+    }
+    def removeParent(node : Int)
+    {
+        m_nodes(node).m_parent = None
+    }
+    
+    def parentOf(node : Int) =
+    {
+        m_nodes(node).m_parent
+    }
+    
+    def setDistance(node : Int, distance : Double)
+    {
+            m_nodes(node).m_distance = distance
+    }
+    
+    def distanceOf(node : Int) =
+    {
+        m_nodes(node).m_distance
+    }
+    
+    def setState(node : Int, state : Color)
+    {
+        m_nodes(node).m_state = state
+    }
+    
+    def stateOf(node : Int) =
+    {
+        m_nodes(node).m_state
+    }
+
+    /*
+        todo - ask Liat why do we need this.
+         We know how the graph will look like - and the order will be by the node number.
+         because each number pointing to every number greater the it.
+     */
+    def topologicalOrder() =
+    {
+        val order = new java.util.ArrayDeque[Int](m_nodes.size)
+        // enter = set(graph)  todo translate
+        def dfs(node : Node)
+        {
+            node.m_state = Color.GRAY
+            for(child <- node.m_neighbors.keySet)
+            {
+                val state = stateOf(child)
+                if(state == Color.GRAY)
+                    throw new RuntimeException("There is a cycle")
+                if(state == Color.WHITE){
+                    //enter.discard(k) todo translate
+                    dfs(m_nodes(child))
+                }
+            }
+            order.addFirst(node.m_id)
+            node.m_state = Color.BLACK
+        }
+        // while enter: dfs(enter.pop()) todo translate
+        order
+    }
+    
+    private case class Node (m_id : Int) extends Ordered[Node]
+    {
+        val m_neighbors = mutable.HashMap[Int, Edge]()
+        var m_parent : Option[Int] = None
+        var m_distance = Double.PositiveInfinity // or destination?
+        var m_state = Color.WHITE
+        
+        // for the sort in the start of topological sort (maybe unnecessary)
+        override def compare(that: Node) = this.m_neighbors.size - that.m_neighbors.size
+    }
+    
+    private case class Edge (m_src : Int, m_dest : Int, m_weight : Double)
+}
